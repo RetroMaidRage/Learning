d00c682583c0062b6d775ec5cb69ed3df577f755
RetroMaidRage <68569682+RetroMaidRage@users.noreply.github.com>
2023-10-24 09:50:50 +0200

    Delete C++/Lab5 directory

--- C++/Lab5/Lab5(10).cpp
+++
@@ -1,4 +0,0 @@
-unsigned long Perimeter(int length, int width) {
-    // Формула для обчислення периметра прямокутника: 2 * (довжина + ширина)
-    return 2UL * (static_cast<unsigned long>(length) + static_cast<unsigned long>(width));
-}
--- C++/Lab5/Lab5(9).cpp
+++
@@ -1,1 +0,0 @@
-unsigned long Perimeter(int length, int width);
